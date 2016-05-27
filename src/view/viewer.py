378c3d29dea8e05d8505255d104ee7b3359c9250
kanairen <turutekab@gmail.com>
2016-05-28 00:00:30 +0900

    add src/view/viewer.py and Viewer class.

---
+++ src/view/viewer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from PyQt4 import QtCore, QtGui, QtOpenGL
+
+
+class Viewer():
+    def __init__(self):
+        pass
+
+    def render(self, obj):
+        raise NotImplementedError
+
+
