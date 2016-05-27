0c03ef8f6eb77cd8ffcbb34fe7ffe2968828cb24
kanairen <turutekab@gmail.com>
2016-05-28 00:00:42 +0900

    add src/view/voxelviewer.py and VoxelViewer class.

---
+++ src/view/voxelviewer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from viewer import Viewer
+
+
+class VoxelViewer(Viewer):
+    def __init__(self):
+        super(VoxelViewer, self).__init__()
+
+    def render(self, voxels):
+        pass
