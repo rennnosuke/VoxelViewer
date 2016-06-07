5672e7ed8c8ef050a33193bcb5b3b1bd929a10c0
kanairen <turutekab@gmail.com>
2016-06-07 19:42:00 +0900

    modify color.pyに新色追加

--- src/util/color.py
+++ src/util/color.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-DARKULA = (43, 43, 43, 255)
+"""
+
+color.py
+
+色ユーティリティモジュール
+
+
+"""
+
+RED = (255, 0, 0, 255)
+
+DARKULA = (60, 63, 65, 255)
+DARKULA_BLUE = (60, 73, 65, 255)
+
+DARKULA_DARK = (43, 43, 43, 255)
+DARKULA_DARK_BLUE= (43, 43, 53, 255)
